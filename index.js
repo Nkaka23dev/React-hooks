c8f73f347f3364b8f614da12451a9855b055fd83
Nkaka23dev <nkakaeric96@gmail.com>
2023-02-22 07:07:05 +0200

    feat: React CDN Practice

---
+++ index.js
@@ -0,0 +1,12 @@
+const nav = (
+    <nav>
+        <h1>Logo Here</h1>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(nav, document.getElementById("root"));
