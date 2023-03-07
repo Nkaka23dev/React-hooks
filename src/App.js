fa9a93b68f3d5fd519cbbfeaeb437a3940a8aa51
Nkaka23dev <nkakaeric96@gmail.com>
2023-03-07 13:35:57 +0200

    feat: add content to useState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
+import { useState } from 'react';
 import './App.css';
-import { data } from './data';
-import Planet from './Planet';
 
 function App() {
+  const [inputField, setInputField] = useState('');
+  const [count, setCount] = useState(0);
+
+  const handleChanges = (e) => {
+    setInputField(e.target.value);
+    console.log(e.target.value)
+  }
+  const handleClick = () => {
+    setCount(prev => prev + 1)
+  }
   return (
-    <div>
-      {data.map((value, key) => {
-        return (
-          <Planet key={key} name={value.name} isGas={value.isGasPlanet} />
-        )
-      })}
-    </div>
+    <>
+      <form>
+        <input type="text" onChange={handleChanges} />
+      </form>
+      <div>
+
+        <div>{inputField}</div>
+        <div>{count}</div>
+        <button onClick={handleClick} >Increment</button>
+      </div>
+    </>
   );
 }
 
