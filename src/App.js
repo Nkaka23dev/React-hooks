624a30243d923f277dafbbe8910d1711d787f5ed
Nkaka23dev <nkakaeric96@gmail.com>
2023-03-09 11:50:00 +0200

    feat: finish useImperativehook

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 import './App.css';
-import UseLayoutEffect from './components/UseLayoutEffect';
-
+import Children from './components/Children';
 
 function App() {
-  const [email, setEmail] = useState("");
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/posts/1/comments").then((data) => {
-      setEmail(data.data[0].email)
-      console.log("AN API IS CALLED RIGHT HERE")
-    }) 
-  },[])
   return (
-    <>
-    {/* <h2>{email}</h2>
-    <h2>{count}</h2>
-    <button onClick={() => {
-      setCount(prev => prev + 1)
-    }} >Change it on click</button> */}
-    <UseLayoutEffect/>
-    </>
+    <section style={{display:"grid"}}>
+    <div style={{margin: "auto"}}>
+    <Children/>
+    </div>
+    </section>
   );
 }
 
